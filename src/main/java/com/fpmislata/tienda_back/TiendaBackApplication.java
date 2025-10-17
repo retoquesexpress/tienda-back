abb9c1467a9335e4e0d6b15feb8ebcebfa8cf27f
Ismael Monleón Rubio <ismael.monleon73@gmail.com>
2025-10-17 13:20:17 +0200

    First Commit

---
+++ src/main/java/com/fpmislata/tienda_back/TiendaBackApplication.java
@@ -0,0 +1,13 @@
+package com.fpmislata.tienda_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpmislata/tienda_back/TiendaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpmislata.tienda_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
